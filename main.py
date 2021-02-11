231f2bf66bc3d0bcde1c3f46e5c93b2a1971a6e9
PorterOBrien2021 <824085@mystma.org>
2021-02-11 15:56:15 +0000

    test

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#WRITE YOUR CODE IN THIS FILE
+
+def removeLast(w):
+    q = w - w[2]
+    for i in range(0, len(w)):
+      return q
+
+print(removeLast("robot"))
+
+
+
+
+
+
+
+
+def firstLast(w):
+    i = len(w)
+    if w[0] == w[i-1]:
+        return True
+    else:
+        return False
+
+print(firstLast("no"))
