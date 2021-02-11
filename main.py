8dd4e9b4dca856fe4e02cb2a77ffe341871bf325
PorterOBrien2021 <824085@mystma.org>
2021-02-11 16:05:09 +0000

    test 2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+def countB(w):
+    count = 0
+    for i in range (0, len(w)):
+        if w[i] == "b":
+            count = (count + 1)
+    return count
+print(countB("baseball"))
 
-def removeLast(w):
-    q = w - w[2]
-    for i in range(0, len(w)):
-      return q
 
-print(removeLast("robot"))
 
 
 
